6dd056fcd1568a8e3b9477f810b7c79ee649af10
jakub <jakub@jakub-VirtualBox>
2017-11-07 21:31:32 +0100

    File tcp.c

---
+++ IPv4/src/tcp.c
@@ -0,0 +1,42 @@
+/*
+ ============================================================================
+ Name        : SendApp.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <netinet/tcp.h>
+#include <netinet/ip.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+unsigned short checksum(const char *buf, unsigned size)
+{
+	unsigned sum = 0;
+	int i;
+
+	/* Accumulate checksum */
+	for (i = 0; i < size - 1; i += 2)
+	{
+		unsigned short word16 = *(unsigned short *) &buf[i];
+		sum += word16;
+	}
+
+	/* Handle odd-sized case */
+	if (size & 1)
+	{
+		unsigned short word16 = (unsigned char) buf[i];
+		sum += word16;
+	}
+
+	/* Fold to get the ones-complement result */
+	while (sum >> 16) sum = (sum & 0xFFFF)+(sum >> 16);
+
+	/* Invert to get the negative in ones-complement arithmetic */
+	return ~sum;
+}
+
